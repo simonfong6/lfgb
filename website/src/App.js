ef4d85ebdb99c9200f6e0dbe008ab35f620e2bf1
Simon <simonfong6@gmail.com>
2021-05-20 15:15:29 -0700

    lfgb ify

--- website/src/App.js
+++ website/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './diamond.png';
 import './App.css';
 
 function App() {
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. 1:38
+          💎🙌🚀🌕
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.coindesk.com/price/bitcoin"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          LFGB
         </a>
       </header>
     </div>
